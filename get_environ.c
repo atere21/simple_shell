1f442cea5e847889a71575488042dd8f9216bbdc
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-11-05 22:26:17 +0100

    Update get_environ.c

--- get_environ.c
+++ get_environ.c
@@ -1,87 +1,92 @@
 #include "shell.h"
 
 /**
- * get_environ - returns the string array copy of our environ
+ * _myenv - prints the current environment
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  * Return: Always 0
  */
-char **get_environ(info_t *info)
+int _myenv(info_t *info)
 {
-{
-info->environ = list_to_strings(info->env);
-info->env_changed = 0;
-}
-
-return (info->environ);
+	print_list_str(info->env);
+	return (0);
 }
 
 /**
- * _unsetenv - Remove an environment variable
+ * _getenv - gets the value of an environ variable
  * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: 1 on delete, 0 otherwise
- * @var: the string env var property
+ * @name: env var name
+ *
+ * Return: the value
  */
-int _unsetenv(info_t *info, char *var)
+char *_getenv(info_t *info, const char *name)
 {
-list_t *node = info->env;
-size_t i = 0;
-char *p;
+	list_t *node = info->env;
+	char *p;
 
-return (0);
-
-while (node)
-{
-p = starts_with(node->str, var);
-if (p && *p == '=')
-{
-info->env_changed = delete_node_at_index(&(info->env), i);
-i = 0;
-node = info->env;
-continue;
-}
-node = node->next;
-i++;
-}
-return (info->env_changed);
+	while (node)
+	{
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
 }
 
 /**
- * _setenv - Initialize a new environment variable,
+ * _mysetenv - Initialize a new environment variable,
  *             or modify an existing one
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
- * @var: the string env var property
- * @value: the string env var value
  *  Return: Always 0
  */
-int _setenv(info_t *info, char *var, char *value)
+int _mysetenv(info_t *info)
 {
-char *buf = NULL;
-list_t *node;
-char *p;
-
-(0);
-
-buf = malloc(_strlen(var) + _strlen(value) + 2);
-(1);
-_strcpy(buf, var);
-_strcat(buf, =);
-_strcat(buf, value);
-node = info->env;
-while (node)
-{
-= starts_with(node->str, var);
-(p && *p == '=')
-ode->str);
-ode->str = buf;
-fo->env_changed = 1;
-(0);
-ode = node->next;
+	if (info->argc != 3)
+	{
+		_eputs("Incorrect number of arguements\n");
+		return (1);
+	}
+	if (_setenv(info, info->argv[1], info->argv[2]))
+		return (0);
+	return (1);
 }
-add_node_end(&(info->env), buf, 0);
-free(buf);
-info->env_changed = 1;
-return (0);
+
+/**
+ * _myunsetenv - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: Always 0
+ */
+int _myunsetenv(info_t *info)
+{
+	int i;
+
+	if (info->argc == 1)
+	{
+		_eputs("Too few arguements.\n");
+		return (1);
+	}
+	for (i = 1; i <= info->argc; i++)
+		_unsetenv(info, info->argv[i]);
+
+	return (0);
+}
+
+/**
+ * populate_env_list - populates env linked list
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
+int populate_env_list(info_t *info)
+{
+	list_t *node = NULL;
+	size_t i;
+
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+	info->env = node;
+	return (0);
 }
