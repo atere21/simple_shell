7b89b27cd0dfa0806671ae875d8edf8362ec6e7c
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-11-05 15:53:07 +0100

    Update errors1.c

--- errors1.c
+++ errors1.c
@@ -8,24 +8,24 @@
  */
 int _erratoi(char *s)
 {
-int i = 0;
-unsigned long int result = 0;
+	int i = 0;
+	unsigned long int result = 0;
 
-if (*s == '+')
-s++;  /* TODO: why does this make main return 255? */
-for (i = 0;  s[i] != '\0'; i++)
-{
-if (s[i] >= '0' && s[i] <= '9')
-{
-result *= 10;
-result += (s[i] - '0');
-if (result > INT_MAX)
-return (-1);
-}
-else
-return (-1);
-}
-return (result);
+	if (*s == '+')
+		s++;  /* TODO: why does this make main return 255? */
+	for (i = 0;  s[i] != '\0'; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += (s[i] - '0');
+			if (result > INT_MAX)
+				return (-1);
+		}
+		else
+			return (-1);
+	}
+	return (result);
 }
 
 /**
@@ -37,13 +37,13 @@ return (result);
  */
 void print_error(info_t *info, char *estr)
 {
-_eputs(info->fname);
-_eputs(: );
-print_d(info->line_count, STDERR_FILENO);
-_eputs(: );
-_eputs(info->argv[0]);
-_eputs(: );
-_eputs(estr);
+	_eputs(info->fname);
+	_eputs(": ");
+	print_d(info->line_count, STDERR_FILENO);
+	_eputs(": ");
+	_eputs(info->argv[0]);
+	_eputs(": ");
+	_eputs(estr);
 }
 
 /**
@@ -55,34 +55,34 @@ _eputs(estr);
  */
 int print_d(int input, int fd)
 {
-int (*__putchar)(char) = _putchar;
-int i, count = 0;
-unsigned int _abs_, current;
+	int (*__putchar)(char) = _putchar;
+	int i, count = 0;
+	unsigned int _abs_, current;
 
-if (fd == STDERR_FILENO)
-__putchar = _eputchar;
-if (input < 0)
-{
-_abs_ = -input;
-__putchar('-');
-count++;
-}
-else
-_abs_ = input;
-current = _abs_;
-for (i = 1000000000; i > 1; i /= 10)
-{
-if (_abs_ / i)
-{
-__putchar('0' + current / i);
-count++;
-}
-current %= i;
-}
-__putchar('0' + current);
-count++;
+	if (fd == STDERR_FILENO)
+		__putchar = _eputchar;
+	if (input < 0)
+	{
+		_abs_ = -input;
+		__putchar('-');
+		count++;
+	}
+	else
+		_abs_ = input;
+	current = _abs_;
+	for (i = 1000000000; i > 1; i /= 10)
+	{
+		if (_abs_ / i)
+		{
+			__putchar('0' + current / i);
+			count++;
+		}
+		current %= i;
+	}
+	__putchar('0' + current);
+	count++;
 
-return (count);
+	return (count);
 }
 
 /**
@@ -95,29 +95,30 @@ return (count);
  */
 char *convert_number(long int num, int base, int flags)
 {
-static char *array;
-static char buffer[50];
-char sign = 0;
-char *ptr;
-unsigned long n = num;
+	static char *array;
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = num;
 
-{
-n = -num;
-sign = '-';
+	if (!(flags & CONVERT_UNSIGNED) && num < 0)
+	{
+		n = -num;
+		sign = '-';
 
-}
-array = flags & CONVERT_LOWERCASE ? 0123456789abcdef : 0123456789ABCDEF;
-ptr = &buffer[49];
-*ptr = '\0';
+	}
+	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
 
-do {
-*--ptr = array[n % base];
-n /= base;
-} while (n != 0);
+	do	{
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
 
-if (sign)
-*--ptr = sign;
-return (ptr);
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
 }
 
 /**
@@ -128,11 +129,12 @@ return (ptr);
  */
 void remove_comments(char *buf)
 {
-int i;
+	int i;
 
-for (i = 0; buf[i] != '\0'; i++)
-{
-buf[i] = '\0';
-break;
-}
+	for (i = 0; buf[i] != '\0'; i++)
+		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
+		{
+			buf[i] = '\0';
+			break;
+		}
 }
