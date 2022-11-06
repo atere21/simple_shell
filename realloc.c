3c71fbf477c1858c135faeaafe774a1abff5a44e
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-11-06 23:12:46 +0100

    Update realloc.c

--- realloc.c
+++ realloc.c
@@ -1,4 +1,4 @@
-#include shell.h
+#include "shell.h"
 
 /**
  **_memset - fills memory with a constant byte
@@ -9,11 +9,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int i;
+	unsigned int i;
 
-for (i = 0; i < n; i++)
-b;
-return (s);
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
 }
 
 /**
@@ -22,11 +22,17 @@ return (s);
  */
 void ffree(char **pp)
 {
-char **a = pp;
+	char **a = pp;
 
-;
-while (*pp)
-* _realloc - reallocates a block of memory
+	if (!pp)
+		return;
+	while (*pp)
+		free(*pp++);
+	free(a);
+}
+
+/**
+ * _realloc - reallocates a block of memory
  * @ptr: pointer to previous malloc'ated block
  * @old_size: byte size of previous block
  * @new_size: byte size of new block
@@ -35,18 +41,21 @@ while (*pp)
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-char *p;
+	char *p;
 
- (malloc(new_size));
- (free(ptr), NULL);
-if (new_size == old_size)
- (ptr);
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(ptr), NULL);
+	if (new_size == old_size)
+		return (ptr);
 
-p = malloc(new_size);
- (NULL);
+	p = malloc(new_size);
+	if (!p)
+		return (NULL);
 
-old_size = old_size < new_size ? old_size : new_size;
-while (old_size--)
-((char *)ptr)[old_size];
-free(ptr);
-return (p);
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		p[old_size] = ((char *)ptr)[old_size];
+	free(ptr);
+	return (p);
