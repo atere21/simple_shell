5864f953c7c8c5d8eb75ec58cb73478f0b8dc1c1
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-11-08 04:26:19 +0100

    Update vars.c

--- vars.c
+++ vars.c
@@ -16,7 +16,7 @@ int is_chain(info_t *info, char *buf, size_t *p)
 	{
 		buf[j] = 0;
 		j++;
-		info->cmd_buf_type = CMD_OR;
+		info->cmd_buf_type = CMD_OR1;
 	}
 	else if (buf[j] == '&' && buf[j + 1] == '&')
 	{
