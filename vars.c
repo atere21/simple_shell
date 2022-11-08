ad57db02497b57a046449062e5e349f4d9baeea1
Atere Oluwatosin <106532533+atere21@users.noreply.github.com>
2022-11-08 04:28:03 +0100

    Update vars.c

--- vars.c
+++ vars.c
@@ -57,7 +57,7 @@ void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 			j = len;
 		}
 	}
-	if (info->cmd_buf_type == CMD_OR)
+	if (info->cmd_buf_type == CMD_OR1)
 	{
 		if (!info->status)
 		{
